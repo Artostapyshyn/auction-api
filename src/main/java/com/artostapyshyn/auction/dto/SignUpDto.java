d981cdab50b0d58ff1d66419b9b98c26d71ffebc
Artostapyshyn <artostapyshyn@gmail.com>
2024-02-11 19:51:15 +0200

    Sign-up fixes

--- src/main/java/com/artostapyshyn/auction/dto/SignUpDto.java
+++ src/main/java/com/artostapyshyn/auction/dto/SignUpDto.java
@@ -1,5 +1,6 @@
 package com.artostapyshyn.auction.dto;
 
+import com.artostapyshyn.auction.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -28,4 +29,6 @@ public class SignUpDto {
     private String city;
 
     private byte[] photoBytes;
+
+    private Role role;
 }
